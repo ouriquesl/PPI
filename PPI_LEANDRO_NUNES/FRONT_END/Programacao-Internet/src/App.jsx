063933d548170e66adaba844c86192632c8e13d3
Leandro Nunes <leandro57740@estudante.ifb.edu.br>
2023-12-13 23:21:36 -0300

    front end atualização

---
+++ PPI_LEANDRO_NUNES/FRONT_END/Programacao-Internet/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Table from './assets/Table'
+import './App.css'
+
+function App() {
+ return (
+  <div>
+    <Table />
+  </div>
+ );
+}
+
+export default App;
---
+++ PPI_LEANDRO_NUNES/FRONT_END/Programacao-Internet/src/assets/Table.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Table = () => {
+ return (
+   <table>
+     <thead>
+       <tr>
+         <th>Nome</th>
+         <th>Email</th>
+         <th>Ações</th>
+       </tr>
+     </thead>
+     <tbody>
+     </tbody>
+   </table>
+ );
+};
+
+export default Table;
